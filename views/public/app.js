4071b0bfe5243ecc4d59e11454be82f484bc1ef3
Ivaylo Marinkov <git@tsarstva.bg>
2016-06-27 01:03:42 +0300

    Initial skeleton

---
+++ views/public/app.js
@@ -0,0 +1,40 @@
+var app;
+(app = function() {
+    "use strict";
+
+    angular.module('li', ['li.services', 'li.controllers', 'ui.router']).
+
+    config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/login ");
+
+        $stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: "views/login/login.html"
+            })
+            .state('dashboard', {
+                url: "/",
+                templateUrl: "views/dashboard/dash.html"
+            })
+            .state('story', {
+                url: "/story",
+                templateUrl: "views/story/story.html",
+            })
+            .state('timeline', {
+                url: "/timeline",
+                templateUrl: "views/timeline/timeline.html",
+            })
+            .state('media', {
+                url: "/media",
+                templateUrl: "views/media/media.html",
+            })
+            .state('admin', {
+                url: "/admin",
+                templateUrl: "views/admin/admin.html",
+            });
+    })
+
+    .run(function($rootScope) {
+        $rootScope.$on("$stateChangeError", console.log.bind(console));
+    });
+})();
